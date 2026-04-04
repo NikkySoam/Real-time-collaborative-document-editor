3c10246b8e4638c74551fce8622f2724f0921e5c
NikkySoam <nikkysoam04@gmail.com>
2026-04-04 09:25:22 +0530

    fix errors facing during deploying

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,42 +8,29 @@
  * @module
  */
 
-import type * as documents from "../documents.js";
-
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
-
-declare const fullApi: ApiFromModules<{
-  documents: typeof documents;
-}>;
+import type * as documents from "../documents.js";
 
 /**
- * A utility for referencing Convex functions in your app's public API.
+ * A utility for referencing Convex functions in your app's API.
  *
  * Usage:
  * ```js
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
+declare const fullApi: ApiFromModules<{
+  documents: typeof documents;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
-
-/**
- * A utility for referencing Convex functions in your app's internal API.
- *
- * Usage:
- * ```js
- * const myFunctionReference = internal.myModule.myFunction;
- * ```
- */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
-
-export declare const components: {};
